1ef0a78006d583b007da67cbf51e42850d64fc1c
Lourdes Debiaggi <ldebiaggi@hotmail.com>
2023-07-27 23:14:24 -0300

    Router de Productos

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -33,33 +33,37 @@ router.get('/:pid', async (req, res) => {
   }
 });
 
-// Endpoint POST /api/products (Permite adiconar un nuevo producto --- REVISAR ME DA QUE FALTAN CAMPOS OBLIGATORIOS CUANDO SE COMPLETAN TODOS)
+// Endpoint POST /api/products (Permite adiconar un nuevo producto --- PRUEBA OK X TC)
 router.post('/', (req, res) => {
-    const product = {
-      title: req.query.title,
-      description: req.query.description,
-      code: req.query.code,
-      price: req.query.price,
-      status: true, // Siguiendo el requerimiento se aplica el status TRUE por defecto
-      stock: req.query.stock,
-      category: req.query.category,
-      thumbnails: req.query.thumbnails ? req.query.thumbnails.split(',') : [], 
-    };
-  
-    const newProduct = productManagerInstance.addProduct(product);
-    if (newProduct) {
-      res.status(201).json(newProduct);
-    } else {
-      res.status(400).json({ error: 'No se pudo agregar el producto' });
-    }
-  });
+  const { title, description, code, price, stock, category, thumbnails } = req.body;
+  const product = {
+    title,
+    description,
+    code,
+    price,
+    status: true, // Siguiendo el requerimiento se aplica el status TRUE por defecto
+    stock: stock,
+    category,
+    thumbnails: thumbnails ? thumbnails.split(',') : [], 
+  };
+
+  const newProduct = productManagerInstance.addProduct(product);
+  if (newProduct) {
+    res.status(201).json(newProduct);
+  } else {
+    res.status(400).json({ error: 'No se pudo agregar el producto' });
+  }
+});
+
 
 // Endpoint PUT /api/products/:pid   (Actualizará un producto) (REVISAR PORQUE ME VOLÓ EL CAMPO)
 router.put('/:pid', async (req, res) => {
   const productId = parseInt(req.params.pid);
-  await productManagerInstance.updateProduct(productId);
+  const updatedTitle = req.body.title; // Obtener el nuevo título del cuerpo de la solicitud
+  await productManagerInstance.updateProduct(productId, updatedTitle);
   res.json({ message: 'Producto actualizado exitosamente' });
 });
+
 
 // Endpoint DELETE /api/products/:pid (Eliminará un producto --- PRUEBA OK X TC)
 router.delete('/:pid', async (req, res) => {
