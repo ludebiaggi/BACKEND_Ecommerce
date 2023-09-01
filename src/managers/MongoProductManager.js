ef5482b61e7e2d441ba15620ba3d4eeb1a1312f0
Lourdes Debiaggi <ldebiaggi@hotmail.com>
2023-09-01 11:33:56 -0300

    SegundaPreEntregaPF___LourdesDebiaggi

--- src/managers/MongoProductManager.js
+++ src/managers/MongoProductManager.js
@@ -26,6 +26,7 @@ class MongoProductManager {
       sort: sortOptions,
       page: page,
       limit: limit,
+      lean: true,
     };
 
     const result = await Product.paginate(queryOptions, options);
